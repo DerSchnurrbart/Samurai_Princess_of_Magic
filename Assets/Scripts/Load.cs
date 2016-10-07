d3f4c98fd205694a8fcc37fc00c46182bfbe9aac
njvorwerck <njvorwerck@gmail.com>
2016-10-07 13:11:35 -0400

    Added basic title screen and placeholder scenes

---
+++ Assets/Scripts/Load.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Load : MonoBehaviour {
+
+    public void loadScene(int SceneNumber)
+    {
+        SceneManager.LoadScene(SceneNumber);
+    }
+
+}
