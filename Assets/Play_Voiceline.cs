e21263d97879af62de233cc0f51133e59692045f
aaleung <aaleung@umich.edu>
2016-11-02 05:09:54 -0400

    Imported Voicelines & Edited Menu
    Voicelines for instructions on how to play, navigate, etc. and lines
    like "Game Over" have been added. A few more will be added as needed as
    beta finalizes. Some lines are extra in case and may or may not be used.
    
    Tweaked main menu UI to be more in line of a radial-menu to improve
    accessibility and to be morein the shape of a mobile screen. Added
    colors and button highlights.

---
+++ Assets/Play_Voiceline.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Play_Voiceline : MonoBehaviour {
+
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+
+    public void playClip()
+    {
+        //not working yet
+        audioSource.clip = Resources.Load("Sounds/Voicelines/Main Menu Instructions/SPoM - Main Menu.wav") as AudioClip; ;
+        audioSource.Play();
+    }
+}
